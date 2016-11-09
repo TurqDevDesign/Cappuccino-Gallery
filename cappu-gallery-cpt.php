6736395956aa7416a33762665650aeec20e98d7b
Austin Fish <TurqDevDesign@Gmail.com>
2016-11-08 19:35:37 -0500

    Improved post edit screen

--- cappu-gallery-cpt.php
+++ cappu-gallery-cpt.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Cappuccino Gallery
 Description: Allows you to easily add videos and images to a gallery, and then use a shortcode to display them on a page of your choice. Optionally include description/captions with each item displayed.
-Version: 0.6
+Version: 0.6.01
 License: GPLv3
 Plugin URI: https://github.com/TurqDevDesign/Cappuccino-Gallery.git
 GitHub Plugin URI: https://github.com/TurqDevDesign/Cappuccino-Gallery.git
